127e474137563ca050d43f5c5d9cdb086bb9387a
Tanzeem Agra <xerxez.in@gmail.com>
2022-10-04 23:21:15 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tanzeem Agra',
+    author='This repository is to demostrate deep learning using mlops',
+    license='MIT',
+)
